b5cbc0839eec1e242ac337c9b21b3a4a3bc1df1d
Sara Bayat Hashemi <s.bayathashemi@yahoo.com>
2023-03-28 00:35:45 -0400

    Added collectables

---
+++ exercise07/Assets/Scripts/Collectables.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Collectables : MonoBehaviour
+{
+    static int score;
+    public TMP_Text scoreText;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            score++;
+            SetScoreText();
+
+            Destroy(this.gameObject);
+
+        }
+
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Marshmallow(s) Collected: " + score;
+    }
+}
